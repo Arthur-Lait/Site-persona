b9383e8d19eab6559e7bfc97f73f0f47e5ad3dec
LAIT <arthurduartedesouza1@gmail.com>
2025-06-04 22:22:43 -0300

    meia correção do php
    meia correção do php, com adição de novos elementos como, produtos e imagens.

---
+++ Site/paginas/home.php
@@ -0,0 +1,46 @@
+<section>
+        <h2>Nossa Empresa</h2>
+        <section class="Mini-Sobre">
+            <div class="left">
+                <img src="./imagens/fotoTeste.jpg">
+            </div>
+            <div class="right">
+                <h5>
+                   <p>sjbdaojihfsjoijshbajidsjdpsuhapdsuhawdefsiuhwbd</p> 
+                    <p>efsoewhdejsdohjwahwjdeihaubhjdfjihuhbdsdwuhefiuh</p>
+                </h5>
+            </div>
+        </section>
+      </section>
+      <section>
+        <div class="produtos">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card h-33">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Produto1</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card h-33">
+                <img src="./imagens/faixada.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Produto2</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur</p>
+                </div>
+              </div>
+            </div>
+            <div class="col md-4">
+              <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Produto3</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
