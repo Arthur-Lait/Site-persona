688603a54818c55d447ba40b4d060b3d38a9c637
LAIT <arthurduartedesouza1@gmail.com>
2025-06-10 22:35:03 -0300

    +css

--- Site/index.php
+++ Site/index.php
@@ -70,7 +70,7 @@
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div class="col-md-4 d-flex align-items-center">
       <a class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap">
-        <img src="imagens/LogoSemFundo.png">
+        <img class="logo-footer" src="imagens/LogoSemFundo.png">
         <svg class="bi" width="30" height="24" aria-hidden="true">
           <use xlink:href="#bootstrap"></use>
         </svg>
--- Site/paginas/home.php
+++ Site/paginas/home.php
@@ -1,13 +1,15 @@
-<section>
-        <h2>Nossa Empresa</h2>
+<section class="titulo">
+        <h3>Nossa Empresa</h3>
         <section class="Mini-Sobre">
             <div class="left">
                 <img src="./imagens/fotoTeste.jpg">
             </div>
             <div class="right">
                 <h5>
-                   <p>sjbdaojihfsjoijshbajidsjdpsuhapdsuhawdefsiuhwbd</p> 
-                    <p>efsoewhdejsdohjwahwjdeihaubhjdfjihuhbdsdwuhefiuh</p>
+                   <p>Somos uma empresa especializada no cuidado e manejo do solo para agricultores
+                     Oferecemos soluções sustentáveis que aumentam a produtividade e preservam o meio ambiente.
+                      Com tecnologia e conhecimento técnico, ajudamos o campo a crescer com saúde e eficiência.</p> 
+                    
                 </h5>
             </div>
         </section>
@@ -17,7 +19,7 @@
           <div class="row">
             <div class="col-md-4">
               <div class="card h-33">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="./imagens/faixada.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Produto1</h5>
                   <p class="card-text">Lorem ipsum dolor sit amet, consectetur</p>
@@ -35,7 +37,7 @@
             </div>
             <div class="col md-4">
               <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="./imagens/faixada.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Produto3</h5>
                   <p class="card-text">Lorem ipsum dolor sit amet, consectetur</p>
